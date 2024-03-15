880bf618066c057472f4dc537009f7e28d825448
zrpy <91083604+zrpy@users.noreply.github.com>
2024-03-15 20:08:57 +0900

    Update member.py

--- member.py
+++ member.py
@@ -1,6 +1,6 @@
 import aiohttp,asyncio,json,time
 
-token=""
+token=""#not bot
 server_id=""
 channel_id=""
 
@@ -8,19 +8,41 @@ async def get_members(token,server,channel):
     session = aiohttp.ClientSession()
     async with session.ws_connect("wss://gateway.discord.gg/?v=10&encoding=json") as ws:
         users=[]
-        async for msg in ws:
+        member_count=0
+        multiple=0
+        channel_user=[[0,99]]
+        count=99
+        async for msg in ws: 
             response=json.loads(msg.data)
+            if response["t"]=="READY":
+                for _ in response["d"]["guilds"]:
+                    member_count=_["member_count"]
             if response["t"]==None:
                 await ws.send_json({"op":2,"d":{"token":token,"capabilities":16381,"properties":{"os":"Android","browser":"Discord Android","device":"Android","system_locale":"ja-JP","browser_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36","browser_version":"122.0.0.0","os_version":"","referrer":"","referring_domain":"","referrer_current":"","referring_domain_current":"","release_channel":"stable","client_build_number":263582,"client_event_source":None},"presence":{"status":"invisible","since":0,"activities":[],"afk":False},"compress":False,"client_state":{"guild_versions":{},"highest_last_message_id":"0","read_state_version":0,"user_guild_settings_version":-1,"private_channels_version":"0","api_code_version":0}}})
             elif response["t"]=="READY_SUPPLEMENTAL":
-                await ws.send_json({"op":14,"d":{"guild_id":server,"typing":True,"activities":True,"threads":True,"channels":{channel:[[0,99],[100,199],[200,299]]}}})
+                await ws.send_json({"op":14,"d":{"guild_id":server,"typing":True,"activities":True,"threads":True,"channels":{channel:[[0,99]]}}})
             elif response["t"]=="GUILD_MEMBER_LIST_UPDATE":
                 for _ in response["d"]["ops"]:
-                    for i in _["items"]:
-                        if "member" in i:
-                            users.append(i["member"]["user"]["id"])
-                await session.close()
-                return users
+                    if _["op"]=="SYNC":
+                        for i in _["items"]:
+                            if "member" in i:
+                                users.append(i["member"]["user"]["id"])
+                    elif _["op"]=="UPDATE":
+                        users.append(_["item"]["member"]["user"]["id"])
+                    if count<member_count:
+                        start_num = int(100*multiple)
+                        end_num=start_num+99
+                        ranges = [[0,99],[start_num, end_num]]
+                        count=end_num
+                        await ws.send_json({"op":14,"d":{"guild_id":server,"typing":True,"activities":True,"threads":True,"channels":{channel:ranges}}})
+                        multiple+=1
+                        time.sleep(0.5)
+                    else:break
+        await session.close()
+        return users
 
 
-print(asyncio.run(get_members(token,server_id,channel_id)))
+async def main():
+    print(await get_members(token,server_id,channel_id))
+
+asyncio.run(main())
